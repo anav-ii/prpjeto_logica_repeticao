27e527ce2a71219b693ae658583383c01852743b
Ana <ana.c.santos611@aluno.senai.br>
2026-06-16 16:54:28 -0300

    salvando processo

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,4 +1,26 @@
 //ESTRUTURA DE REPETIÇÃO FOR
+const divFor = document.querySelector('#div-for')
 for(i = 0;i < 10; i++){
     console.log(i,'Amor')
+  divFor.innerHTML += `${i} - Amor <br>`
 }
+
+
+//CONTADOR/ACUMULADOR
+const inputnum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContacum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click', (evt)=>{
+  let numdigitado = Number(input.value)
+
+  cont++
+  acum += numDigitado
+divContacum.innerHTML = `TOTAL DE NÚMERO É: ${cont} <br>
+ A SOMA DOS NÚMEROS É: ${acum}`
+
+ inputnum.value = ''
+})
